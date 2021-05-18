2aad1587f8bfd3d2f9e2a45c49404bb0443e86b1
Gerson Umanzor <gbumanzor@outlook.com>
2021-05-17 18:13:38 -0600

    Feat: Start working on the YuGiOh.

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,42 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class='container'>
+    <Navbar />
+    <div class='content'>
+      <router-view />
+    </div>
   </div>
-  <router-view/>
 </template>
 
+<script>
+
+import Navbar from '@/components/Navbar';
+
+export default {
+  name: 'App',
+  components: { Navbar },
+};
+</script>
+
 <style>
+body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.container {
+  display: flex;
+  flex-direction: column;
+}
+
+.content {
+  padding: 24px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
