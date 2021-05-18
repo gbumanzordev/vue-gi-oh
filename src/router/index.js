4a2f3fa3649c83d06bed3ab7a32f907156047c8a
Gerson Umanzor <gbumanzor@outlook.com>
2021-05-17 18:13:51 -0600

    Feat: Add router

---
+++ src/router/index.js
@@ -0,0 +1,31 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '../views/Home.vue';
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: () => import('../views/About'),
+  },
+  {
+    path: '/contact',
+    name: 'Contact',
+    component: () => import('../views/Contact'),
+  },
+  {
+    path: '/:id',
+    component: () => import('../views/Details'),
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
