7eb07d0b6dbd4b5455cec81ac2c8610176368578
Gerson Umanzor <gbumanzor@outlook.com>
2021-05-23 21:28:01 -0600

    Feat: Add about info.

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,37 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About me</h1>
+    <p>Hi there! 👋</p>
+    <p>My name is Gerson Umanzor and I'm from El Salvador 💙.</p>
+
+    <p>🔭 I'm currently working at Applaudo Studios as a Frontend Developer.</p>
+
+    <p>
+      🌱 I’m currently using Angular Developer as my main Technology and also
+      working on improving my Vue skills. I love reading articles and learning
+      new stuff. You can check my repository collection and if you have
+      suggestions make sure to leave comments or contact me. I'm also trying to
+      build reusable packages I believe the community can find useful.
+    </p>
+
+    <p>
+      📫 You can reach me through any of the links I left in the
+      <router-link to="/contact">contact</router-link> page. You can also submit
+      pull requests to the packages I built to make improvements. You can also
+      reach out in my social:
+    </p>
+
+    <h1 class="about-the-app">About the App</h1>
+    <p>Content to be added once the app is finished</p>
   </div>
 </template>
+<style scoped>
+p {
+  line-height: 1.8rem;
+  font-size: 18px;
+}
+
+.about-the-app {
+  margin-top: 64px;
+}
+</style>
