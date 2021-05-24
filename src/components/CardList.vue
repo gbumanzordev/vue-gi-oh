3e8852dc6364057ada50a8e68092aff1d8ce5217
Gerson Umanzor <gbumanzor@outlook.com>
2021-05-23 20:48:25 -0600

    Feat: Import observer and listen when scrolled to load more items.

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,17 +1,47 @@
 <template>
   <div class="list">
-    <Card v-for="card in cards" :key="card.id" :card="card" />
+    <Card v-for="card in displayed" :key="card.id" :card="card" />
+    <Observer @scrolled="loadMore" v-if="cards.length" />
   </div>
 </template>
 
 <script>
 import Card from '@/components/Card';
+import Observer from '@/components/Observer';
 
 export default {
   name: 'CardList',
-  components: { Card },
+  components: { Observer, Card },
   props: {
     cards: Array,
+  },
+  watch: {
+    cards: function (newValue, oldValue) {
+      this.page = 1;
+      if (newValue !== oldValue) {
+        this.displayed = this.cards.slice(0, this.listSize);
+      }
+    },
+  },
+  data() {
+    return {
+      displayed: [],
+      listSize: 50,
+      page: 1,
+    };
+  },
+  mounted() {
+    if (this.cards.length) {
+      this.displayed = this.cards.slice(0, this.listSize);
+    }
+  },
+  methods: {
+    loadMore() {
+      if (this.cards.length - this.listSize * this.page >= this.listSize) {
+        this.page = this.page + 1;
+        this.displayed = [...this.cards.slice(0, this.listSize * this.page)];
+      }
+    },
   },
 };
 </script>
