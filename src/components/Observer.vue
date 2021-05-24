74cf1cab1c3b1e5f8c89d2f33d6f6ebf62b72498
Gerson Umanzor <gbumanzor@outlook.com>
2021-05-23 20:47:32 -0600

    Feat: Create Observer component to listen when the item has been scrolled to.

---
+++ src/components/Observer.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="observer"></div>
+</template>
+
+<script>
+export default {
+  name: 'Observer',
+  emits: ['scrolled'],
+  data() {
+    return {
+      observer: null,
+    };
+  },
+  mounted() {
+    this.observer = new IntersectionObserver(([entry]) => {
+      if (entry && entry.isIntersecting) {
+        this.$emit('scrolled');
+      }
+    });
+
+    this.observer.observe(this.$el);
+  },
+};
+</script>
+
+<style scoped></style>
